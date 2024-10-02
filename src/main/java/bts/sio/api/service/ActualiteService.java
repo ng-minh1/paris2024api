f2d82a8ccef3a304fcbdd3e6e0e5dadb60dab62b
Minh-Tri <nguyen.minhtri@outlook.fr>
2024-10-02 09:17:15 +0200

    Création de ActualiteService

---
+++ src/main/java/bts/sio/api/service/ActualiteService.java
@@ -0,0 +1,30 @@
+package bts.sio.api.service;
+
+import bts.sio.api.model.Actualite;
+import bts.sio.api.repository.ActualiteRepository;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Data
+@Service
+public class ActualiteService {
+    @Autowired
+    private ActualiteRepository actualiteRepository;
+
+    public Optional<Actualite> getActualite(final Long id) { return actualiteRepository.findById(id); }
+
+    public Iterable<Actualite> getActualites() {
+        return actualiteRepository.findAll();
+    }
+
+    public void deleteActualite(final Long id) {
+        actualiteRepository.deleteById(id);
+    }
+
+    public Actualite saveActualite(Actualite actualite) {
+        Actualite savedActualite = actualiteRepository.save(actualite);
+        return savedActualite;
+    }
+}
