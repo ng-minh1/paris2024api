4fd5a8824af7e3a31b032bc897b8f60c22b86beb
Minh-Tri <nguyen.minhtri@outlook.fr>
2024-10-02 09:55:33 +0200

    Modifictation pour pouvoir lister les athletes

--- src/main/java/bts/sio/api/controller/ActualiteController.java
+++ src/main/java/bts/sio/api/controller/ActualiteController.java
@@ -15,17 +15,6 @@ public class ActualiteController {
     private ActualiteService actualiteService;
 
     /**
-     * Create - Add a new actualite
-     * @param actualite An object actualite
-     * @return The actualite object saved
-     */
-    @PostMapping("/actualite")
-    public Actualite createActualite(@RequestBody Actualite actualite) {
-        return actualiteService.saveActualite(actualite);
-    }
-
-
-    /**
      * Read - Get one actualite
      * @param id The id of the actualite
      * @return An Actualite object full filled
@@ -47,48 +36,6 @@ public class ActualiteController {
     @GetMapping("/actualites")
     public Iterable<Actualite> getActualites() {
         return actualiteService.getActualites();
-    }
-
-    /**
-     * Update - Update an existing actualite
-     * @param id - The id of the actualite to update
-     * @param actualite - The actualite object updated
-     * @return
-     */
-    @PutMapping("/actualite/{id}")
-    public Actualite updateActualite(@PathVariable("id") final Long id, @RequestBody Actualite actualite) {
-        Optional<Actualite> e = actualiteService.getActualite(id);
-        if(e.isPresent()) {
-            Actualite currentActualite = e.get();
-
-            String titre = actualite.getTitre();
-            if(titre != null) {
-                currentActualite.setTitre(titre);
-            }
-            String contenu = actualite.getContenu();
-            if(contenu != null) {
-                currentActualite.setContenu(contenu);;
-            }
-            Sport sport = actualite.getSport();
-            if(sport != null) {
-                currentActualite.setSport(sport);;
-            }
-
-            actualiteService.saveActualite(currentActualite);
-            return currentActualite;
-        } else {
-            return null;
-        }
-    }
-
-
-    /**
-     * Delete - Delete an actualite
-     * @param id - The id of the actualite to delete
-     */
-    @DeleteMapping("/actualite/{id}")
-    public void deleteActualite(@PathVariable("id") final Long id) {
-        actualiteService.deleteActualite(id);
     }
 
 }
