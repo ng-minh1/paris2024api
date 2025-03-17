e4f4177c88f095e2e66d5c692ba652b99ebfa064
Minh-Tri <nguyen.minhtri@outlook.fr>
2025-03-17 16:42:37 +0100

    Ajout des fonctions permettant de faire le lien d'athlète et sport

--- src/main/java/bts/sio/api/repository/AthleteRepository.java
+++ src/main/java/bts/sio/api/repository/AthleteRepository.java
@@ -4,7 +4,10 @@ import bts.sio.api.model.Athlete;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AthleteRepository extends CrudRepository<Athlete, Long> {
+    List<Athlete> findBySportId(Long sportId);
 
 }
