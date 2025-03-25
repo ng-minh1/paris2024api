55df93472837d84ef65891cbe60c8fa6ecb85191
Minh-Tri <nguyen.minhtri@outlook.fr>
2025-03-25 15:21:37 +0100

    ajout de la méthode getPays

--- src/main/java/bts/sio/api/controller/PaysController.java
+++ src/main/java/bts/sio/api/controller/PaysController.java
@@ -1,10 +1,14 @@
 package bts.sio.api.controller;
 
 import bts.sio.api.model.Pays;
+import bts.sio.api.model.Sport;
 import bts.sio.api.service.PaysService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class PaysController {
@@ -19,5 +23,15 @@ public class PaysController {
     @GetMapping("/pays")
     public Iterable<Pays> getAthletes() {
         return paysService.getLesPays();
+    }
+
+    @GetMapping("/pays/{id}")
+    public Pays getPays(@PathVariable("id") final Long id) {
+        Optional<Pays> pays = paysService.getPays(id);
+        if(pays.isPresent()) {
+            return pays.get();
+        } else {
+            return null;
+        }
     }
 }
